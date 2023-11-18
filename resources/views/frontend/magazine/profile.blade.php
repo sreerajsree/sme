e43306a297039d01ecfb977dd700589b3f849b1c
sreerajsree <sreeandsreeraj@gmail.com>
2023-11-18 12:49:30 +0530

    ads mew

--- resources/views/frontend/magazine/profile.blade.php
+++ resources/views/frontend/magazine/profile.blade.php
@@ -86,6 +86,9 @@
                             </div>
                         @endforeach
                     </div>
+                    <a href="">
+                        <img src="{{ asset('logo/Fami_ly-Promoise.png') }}" alt="">
+                    </a>
                 </div>
             </div>
         </div>
