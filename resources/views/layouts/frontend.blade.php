cc1b3c8c1eabecdeeb4f5c0dc7df9140465cd231
sreerajsree <sreeandsreeraj@gmail.com>
2023-12-16 13:55:47 +0530

    show page 5"

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -116,7 +116,7 @@
                 infinite: true,
                 speed: 300,
                 autoplay: true,
-                slidesToShow: 6,
+                slidesToShow: 6.2,
                 slidesToScroll: 1,
                 vertical: true,
                 verticalSwiping: true,
