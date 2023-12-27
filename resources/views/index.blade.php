28d54691a545348922ff9fa49839af1c4b373417
sreerajsree <sreeandsreeraj@gmail.com>
2023-12-28 01:12:32 +0530

    side slider new 4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -497,7 +497,7 @@
                 infinite: true,
                 speed: 300,
                 autoplay: true,
-                slidesToShow: 7,
+                slidesToShow: 5,
                 slidesToScroll: 1,
                 vertical: true,
                 verticalSwiping: true,
