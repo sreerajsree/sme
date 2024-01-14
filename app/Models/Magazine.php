16c3832f2701a1c0f817e1f6af3f6b071abbbf09
sreerajsree <sreeandsreeraj@gmail.com>
2024-01-15 01:34:05 +0530

    mag edits 14

--- app/Models/Magazine.php
+++ app/Models/Magazine.php
@@ -24,6 +24,7 @@ class Magazine extends Model
         $magazines = Magazine::where('month', '=', $month)
         ->where('year', '=', $year)
         ->where('published', '=', 1)
+        ->orderBy('date', 'DESC')
         ->get();
         return $magazines;
     }
